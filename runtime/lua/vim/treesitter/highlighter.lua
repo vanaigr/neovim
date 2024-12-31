dc0cdb01b370baa3602bc323c23b1e54a9906cb9
vanaigr <vanaigranov@gmail.com>
2024-12-30 23:29:14 -0600

    fix: draw child injections always on top

--- runtime/lua/vim/treesitter/highlighter.lua
+++ runtime/lua/vim/treesitter/highlighter.lua
@@ -4,12 +4,36 @@ local Range = require('vim.treesitter._range')
 
 local ns = api.nvim_create_namespace('treesitter/highlighter')
 
+
+---@param priorities { [1]: integer, [2]: integer }
+local function add_priority(priorities, pred)
+  if pred[1] ~= 'set!' or pred[2] ~= 'priority' then
+    return
+  end
+
+  ---@type integer
+  local priority
+  if type(pred[3]) == 'string' then
+    priority = tonumber(pred[3])
+  else
+    priority = tonumber(pred[4])
+  end
+
+  if priorities[1] > priority then
+    priorities[1] = priority
+  end
+  if priorities[2] < priority then
+    priorities[2] = priority
+  end
+end
+
 ---@alias vim.treesitter.highlighter.Iter fun(end_line: integer|nil): integer, TSNode, vim.treesitter.query.TSMetadata, TSQueryMatch
 
 ---@class (private) vim.treesitter.highlighter.Query
 ---@field private _query vim.treesitter.Query?
 ---@field private lang string
 ---@field private hl_cache table<integer,integer>
+---@field priorities { [1]: integer, [2]: integer }
 local TSHighlighterQuery = {}
 TSHighlighterQuery.__index = TSHighlighterQuery
 
@@ -27,6 +51,19 @@ function TSHighlighterQuery.new(lang, query_string)
   else
     self._query = query.get(lang, 'highlights')
   end
+
+  local priorities = { vim.hl.priorities.treesitter, vim.hl.priorities.treesitter }
+
+  local patterns = self._query.info.patterns
+  for _, preds in pairs(patterns) do
+    for _, pred in ipairs(preds) do
+      add_priority(priorities, pred)
+    end
+  end
+
+  -- for spell offset
+  priorities[2] = priorities[2] + 1
+  self.priorities = priorities
 
   return self
 end
@@ -52,12 +89,6 @@ function TSHighlighterQuery:query()
   return self._query
 end
 
----@class (private) vim.treesitter.highlighter.State
----@field tstree TSTree
----@field next_row integer
----@field iter vim.treesitter.highlighter.Iter?
----@field highlighter_query vim.treesitter.highlighter.Query
-
 ---@nodoc
 ---@class vim.treesitter.highlighter
 ---@field active table<integer,vim.treesitter.highlighter>
@@ -65,7 +96,7 @@ end
 ---@field private orig_spelloptions string
 --- A map of highlight states.
 --- This state is kept during rendering across each line update.
----@field private _highlight_states vim.treesitter.highlighter.State[]
+---@field private _highlight_state vim.treesitter.highlighter.State
 ---@field private _queries table<string,vim.treesitter.highlighter.Query>
 ---@field tree vim.treesitter.LanguageTree
 ---@field private redraw_count integer
@@ -166,49 +197,90 @@ function TSHighlighter:destroy()
   end
 end
 
+---@class (private) vim.treesitter.highlighter.TreeInfo
+---@field tstree TSTree
+---@field next_row integer
+---@field iter vim.treesitter.highlighter.Iter
+---@field second_layer_nodes TSNode[]
+
+---@class (private) vim.treesitter.highlighter.TreesInfo
+---@field highlighter_query vim.treesitter.highlighter.Query
+---@field priorities { [1]: integer, [2]: integer }[]
+---@field trees vim.treesitter.highlighter.TreeInfo[]
+
+---@class (private) vim.treesitter.highlighter.State
+---@field total_priorities integer
+---@field trees_info? vim.treesitter.highlighter.TreesInfo
+---@field child_states vim.treesitter.highlighter.State[]
+---@field child_off integer
+
+---@param langtree vim.treesitter.LanguageTree
+---@param opts table
+---@return vim.treesitter.highlighter.State
+function TSHighlighter:compute_hl_state(langtree, opts)
+  local max_child_priorities = 0
+  local child_states = {}
+  for _, child in pairs(langtree._children) do
+    local state = self:compute_hl_state(child, opts)
+    max_child_priorities = math.max(max_child_priorities, state.total_priorities)
+    table.insert(child_states, state)
+  end
+
+  local total_priorities = max_child_priorities
+  local child_off = 0
+
+  local query = self:get_query(langtree:lang())
+
+  ---@type vim.treesitter.highlighter.TreesInfo
+  local trees_info
+  if query then
+    local trees = {}
+    for _, tstree in pairs(langtree._trees) do
+      if tstree then
+        local root_node = tstree:root()
+        local root_start_row, _, root_end_row, _ = root_node:range()
+        if root_end_row >= opts.srow and root_start_row <= opts.erow then
+          table.insert(trees, {
+            tstree = tstree,
+            next_row = 0,
+            iter = nil,
+            second_layer_nodes = {},
+          })
+        end
+      end
+    end
+
+    if #trees ~= 0 then
+      local priorities = query.priorities
+      local priorities_count = (priorities[2] - priorities[1] + 1)
+
+      total_priorities = total_priorities + priorities_count
+
+      trees_info = {
+        highlighter_query = query,
+        priorities = priorities,
+        trees = trees,
+      }
+      child_off = priorities_count
+    end
+  end
+
+  return {
+    total_priorities = total_priorities,
+    trees_info = trees_info,
+    child_states = child_states,
+    child_off = child_off,
+  }
+end
+
 ---@param srow integer
 ---@param erow integer exclusive
 ---@private
 function TSHighlighter:prepare_highlight_states(srow, erow)
-  self._highlight_states = {}
-
-  self.tree:for_each_tree(function(tstree, tree)
-    if not tstree then
-      return
-    end
-
-    local root_node = tstree:root()
-    local root_start_row, _, root_end_row, _ = root_node:range()
-
-    -- Only consider trees within the visible range
-    if root_start_row > erow or root_end_row < srow then
-      return
-    end
-
-    local highlighter_query = self:get_query(tree:lang())
-
-    -- Some injected languages may not have highlight queries.
-    if not highlighter_query:query() then
-      return
-    end
-
-    -- _highlight_states should be a list so that the highlights are added in the same order as
-    -- for_each_tree traversal. This ensures that parents' highlight don't override children's.
-    table.insert(self._highlight_states, {
-      tstree = tstree,
-      next_row = 0,
-      iter = nil,
-      highlighter_query = highlighter_query,
-    })
-  end)
-end
-
----@param fn fun(state: vim.treesitter.highlighter.State)
----@package
-function TSHighlighter:for_each_highlight_state(fn)
-  for _, state in ipairs(self._highlight_states) do
-    fn(state)
-  end
+  self._highlight_state = self:compute_hl_state(
+    self.tree,
+    { srow = srow, erow = erow }
+  )
 end
 
 ---@package
@@ -276,75 +348,97 @@ local function get_spell(capture_name)
   return nil, 0
 end
 
+---@param trees vim.treesitter.highlighter.TreesInfo
+---@param state vim.treesitter.highlighter.TreeInfo
+---@param priority_off integer
+---@param second_layer table[]
+---@param opts table
+local function hl_tree(trees, state, priority_off, second_layer, opts)
+  local root_node = state.tstree:root()
+  local root_start_row, _, root_end_row, _ = root_node:range()
+
+  -- Only consider trees that contain this line
+  if root_start_row > opts.line or root_end_row < opts.line then
+    return
+  end
+
+  if state.iter == nil or state.next_row < opts.line then
+    -- Mainly used to skip over folds
+
+    -- TODO(lewis6991): Creating a new iterator loses the cached predicate results for query
+    -- matches. Move this logic inside iter_captures() so we can maintain the cache.
+    state.iter = trees.highlighter_query:query():iter_captures(root_node, opts.buf, opts.line, root_end_row + 1)
+  end
+
+  while opts.line >= state.next_row do
+    local capture, node, metadata, match = state.iter(opts.line)
+
+    local range = { root_end_row + 1, 0, root_end_row + 1, 0 }
+    if node then
+      range = vim.treesitter.get_range(node, opts.buf, metadata and metadata[capture])
+    end
+    local start_row, start_col, end_row, end_col = Range.unpack4(range)
+
+    if capture then
+      local hl = trees.highlighter_query:get_hl_from_capture(capture)
+
+      local capture_name = trees.highlighter_query:query().captures[capture]
+
+      local spell, spell_pri_offset = get_spell(capture_name)
+
+      -- The "priority" attribute can be set at the pattern level or on a particular capture
+      local priority = (
+        tonumber(metadata.priority or metadata[capture] and metadata[capture].priority)
+        or vim.hl.priorities.treesitter
+      ) + spell_pri_offset
+
+      -- The "conceal" attribute can be set at the pattern level or on a particular capture
+      local conceal = metadata.conceal or metadata[capture] and metadata[capture].conceal
+      local url = get_url(match, opts.buf, capture, metadata)
+
+      print(priority - trees.priorities[1] + priority_off)
+      if hl and end_row >= opts.line and (not opts.is_spell_nav or spell ~= nil) then
+        api.nvim_buf_set_extmark(opts.buf, ns, start_row, start_col, {
+          end_line = end_row,
+          end_col = end_col,
+          hl_group = hl,
+          ephemeral = true,
+          priority = priority - trees.priorities[1] + priority_off,
+          conceal = conceal,
+          spell = spell,
+          url = url,
+        })
+      end
+    end
+
+    if start_row > opts.line then
+      state.next_row = start_row
+    end
+  end
+end
+
+---@param state vim.treesitter.highlighter.State
+---@param priority_off integer
+---@param opts table
+local function highlight(state, priority_off, opts)
+  print('--', priority_off)
+  local second_layer = {}
+  for _, tstree in ipairs(state.trees_info.trees) do
+    hl_tree(state.trees_info, tstree, priority_off, second_layer, opts)
+  end
+  local child_priority_off = priority_off + state.child_off
+  for _, child in ipairs(state.child_states) do
+    highlight(child, child_priority_off, opts)
+  end
+end
+
 ---@param self vim.treesitter.highlighter
 ---@param buf integer
 ---@param line integer
 ---@param is_spell_nav boolean
 local function on_line_impl(self, buf, line, is_spell_nav)
-  self:for_each_highlight_state(function(state)
-    local root_node = state.tstree:root()
-    local root_start_row, _, root_end_row, _ = root_node:range()
-
-    -- Only consider trees that contain this line
-    if root_start_row > line or root_end_row < line then
-      return
-    end
-
-    if state.iter == nil or state.next_row < line then
-      -- Mainly used to skip over folds
-
-      -- TODO(lewis6991): Creating a new iterator loses the cached predicate results for query
-      -- matches. Move this logic inside iter_captures() so we can maintain the cache.
-      state.iter =
-        state.highlighter_query:query():iter_captures(root_node, self.bufnr, line, root_end_row + 1)
-    end
-
-    while line >= state.next_row do
-      local capture, node, metadata, match = state.iter(line)
-
-      local range = { root_end_row + 1, 0, root_end_row + 1, 0 }
-      if node then
-        range = vim.treesitter.get_range(node, buf, metadata and metadata[capture])
-      end
-      local start_row, start_col, end_row, end_col = Range.unpack4(range)
-
-      if capture then
-        local hl = state.highlighter_query:get_hl_from_capture(capture)
-
-        local capture_name = state.highlighter_query:query().captures[capture]
-
-        local spell, spell_pri_offset = get_spell(capture_name)
-
-        -- The "priority" attribute can be set at the pattern level or on a particular capture
-        local priority = (
-          tonumber(metadata.priority or metadata[capture] and metadata[capture].priority)
-          or vim.hl.priorities.treesitter
-        ) + spell_pri_offset
-
-        -- The "conceal" attribute can be set at the pattern level or on a particular capture
-        local conceal = metadata.conceal or metadata[capture] and metadata[capture].conceal
-
-        local url = get_url(match, buf, capture, metadata)
-
-        if hl and end_row >= line and (not is_spell_nav or spell ~= nil) then
-          api.nvim_buf_set_extmark(buf, ns, start_row, start_col, {
-            end_line = end_row,
-            end_col = end_col,
-            hl_group = hl,
-            ephemeral = true,
-            priority = priority,
-            conceal = conceal,
-            spell = spell,
-            url = url,
-          })
-        end
-      end
-
-      if start_row > line then
-        state.next_row = start_row
-      end
-    end
-  end)
+  local opts = { self = self, buf = buf, line = line, is_spell_nav = is_spell_nav }
+  highlight(self._highlight_state, vim.highlight.priorities.treesitter, opts)
 end
 
 ---@private
